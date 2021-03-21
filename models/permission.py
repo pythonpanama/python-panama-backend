214f86b1c83ce805e6a187aeae10a03b5617db75
Javier Feliu <javier@wanderin.dev>
2021-03-21 15:13:46 -0500

    feat: Add PermissionModel

---
+++ models/permission.py
@@ -0,0 +1,17 @@
+from db import db
+from models.model_mixin import ModelMixin
+
+
+class PermissionModel(db.Model, ModelMixin):
+    __tablename__ = "permissions"
+
+    id = db.Column(db.Integer, primary_key=True)
+    permission_name = db.Column(db.String(50), nullable=False, unique=True)
+
+    @classmethod
+    def find_by_id(cls, id: int) -> "PermissionModel":
+        return cls.query.filter_by(id=id).first()
+
+    @classmethod
+    def find_by_name(cls, permission_name: str) -> "PermissionModel":
+        return cls.query.filter_by(permission_name=permission_name).first()

eee9949e4863cf8bd595f396655bdf36421cdf79
Javier Feliu <javier@wanderin.dev>
2021-03-24 18:12:32 -0500

    chore: Add test data for models

---
+++ tests/model_test_data.py
@@ -0,0 +1,52 @@
+TEST_KEYNOTE = {
+    "title": "Uso de type hints en Python",
+    "description": "Que son los type hints y por qué debemos usarlos",
+    "speaker_id": 1,
+    "meeting_id": 1,
+}
+
+TEST_MEMBER = {
+    "email": "jperez@ppty.com",
+    "password": "pass",
+    "mobile_phone": "+50769876543",
+    "first_name": "Juan",
+    "last_name": "Pérez",
+    "linkedin_profile": "https://linkedin.com/in/juan_perez",
+    "github_profile": "https://github.com/juan_perez",
+    "twitter_profile": "https://twitter.com/juan_perez",
+    "profile_picture": "https://ppty.com/img/D6e6lKNRCbb4RXs6.png",
+    "is_active": True,
+    "role_id": 1,
+}
+
+TEST_MEETING = {
+    "datetime": "2021-03-31 20:00:00",
+    "type": "online",
+    "location": "https://www.meetup.com/Python-Panama/events/276661559",
+    "description": "Python Meetup Vol. 25",
+    "creator_id": 1,
+}
+
+TEST_PERMISSION = {"permission_name": "post:project"}
+
+TEST_PROJECT = {
+    "start_date": "2021-03-16",
+    "end_date": "2021-04-15",
+    "title": "REST API para sitio web",
+    "description": "Un API en Python/Flask para conectar el frontend a la base de datos",
+    "goals": "Generar páginas de forma dinamica para miembros, reuniones y proyectos",
+    "status": "in progress",
+    "admin_id": 1,
+}
+
+TEST_ROLE = {"role_name": "admin"}
+
+TEST_SPEAKER = {
+    "first_name": "Tomás",
+    "last_name": "González",
+    "linkedin_profile": "https://linkedin.com/in/tomas_gonzalez",
+    "github_profile": "https://github.com/tomas_gonzalez",
+    "twitter_profile": "https://twitter.com/tomas_gonzalez",
+    "bio": "Experto en Python y el uso de type hints",
+    "profile_picture": "https://ppty.com/img/hA4oCfR&o17mqsXm.png",
+}

ad3164600bec6f66de365559c82ec1eafce57d09
Javier Feliu <javier@wanderin.dev>
2021-04-08 16:28:44 -0500

    refactor: Abstract out repetitive code and hard coded values from tests for the meeting model and resource

--- tests/models/test_meeting.py
+++ tests/models/test_meeting.py
@@ -3,66 +3,67 @@ import unittest
 from models.meeting import MeetingModel
 from models.member import MemberModel
 from tests.base_test import BaseTest
+from tests.model_test_data import TEST_MEETING_1
 
 
 class TestMeeting(BaseTest):
     """Test all methods for the MeetingModel"""
 
+    DATETIME = TEST_MEETING_1["datetime"]
+    TYPE = TEST_MEETING_1["type"]
+    LOCATION = TEST_MEETING_1["location"]
+    DESCRIPTION = TEST_MEETING_1["description"]
+    CREATOR_ID = TEST_MEETING_1["creator_id"]
+
     def test_init(self):
         with self.app_context:
-            self.assertEqual(self.meeting_1.datetime, "2021-03-31 20:00:00")
-            self.assertEqual(self.meeting_1.type, "online")
-            self.assertEqual(
-                self.meeting_1.location,
-                "https://www.meetup.com/Python-Panama/events/276661559",
-            )
-            self.assertEqual(
-                self.meeting_1.description, "Python Meetup Vol. 25"
-            )
-            self.assertEqual(self.meeting_1.creator_id, 1)
+            self.assertEqual(self.meeting_1.datetime, self.DATETIME)
+            self.assertEqual(self.meeting_1.type, self.TYPE)
+            self.assertEqual(self.meeting_1.location, self.LOCATION)
+            self.assertEqual(self.meeting_1.description, self.DESCRIPTION)
+            self.assertEqual(self.meeting_1.creator_id, self.CREATOR_ID)
 
     def test_find_all(self):
         with self.app_context:
-            self.role_1.save_to_db()
-            self.member_1.save_to_db()
-            meeting_id = self.meeting_1.save_to_db().id
+            meeting, _, _ = self.add_meeting_to_db(
+                self.meeting_1, self.member_1, self.role_1
+            )
 
             meetings = MeetingModel.find_all()
 
-            self.assertEqual(meetings[0].id, meeting_id)
+            self.assertEqual(meetings[0].id, meeting.id)
 
     def test_find_by_creator_id(self):
         with self.app_context:
-            self.role_1.save_to_db()
-            creator_id = self.member_1.save_to_db().id
-            meeting_id = self.meeting_1.save_to_db().id
+            meeting, creator, _ = self.add_meeting_to_db(
+                self.meeting_1, self.member_1, self.role_1
+            )
 
-            meetings = MeetingModel.find_by_creator_id(creator_id)
+            meetings = MeetingModel.find_by_creator_id(creator.id)
 
-            self.assertEqual(meetings[0].id, meeting_id)
+            self.assertEqual(meetings[0].id, meeting.id)
 
     def test_find_by_id(self):
         with self.app_context:
-            self.role_1.save_to_db()
-            self.member_1.save_to_db()
-            meeting_id = self.meeting_1.save_to_db().id
+            meeting, _, _ = self.add_meeting_to_db(
+                self.meeting_1, self.member_1, self.role_1
+            )
 
-            meeting = MeetingModel.find_by_id(meeting_id)
+            meeting = MeetingModel.find_by_id(meeting.id)
 
-            self.assertEqual(meeting.description, "Python Meetup Vol. 25")
+            self.assertEqual(meeting.description, self.DESCRIPTION)
 
     def test_meetings_members_relation(self):
         with self.app_context:
-            self.role_1.save_to_db()
-            self.member_1.save_to_db()
-            meeting_id = self.meeting_1.save_to_db().id
-            member_id = self.member_1.save_to_db().id
+            meeting, member, _ = self.add_meeting_to_db(
+                self.meeting_1, self.member_1, self.role_1
+            )
 
-            meeting = MeetingModel.find_by_id(meeting_id)
-            member = MemberModel.find_by_id(member_id)
+            meeting = MeetingModel.find_by_id(meeting.id)
+            member = MemberModel.find_by_id(member.id)
             meeting.members.append(member)
 
-            self.assertEqual(meeting.members[0].email, "jperez@ppty.com")
+            self.assertEqual(meeting.members[0].email, member.email)
 
 
 if __name__ == "__main__":  # pragma: no cover

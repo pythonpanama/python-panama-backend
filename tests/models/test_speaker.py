83427d27e6b5ff5ad20cb792d2a3b76cf6baa54f
Javier Feliu <javier@wanderin.dev>
2021-04-14 11:38:43 -0500

    refactor: Abstract out repetitive code and hard coded values from tests for the speaker model and resource

--- tests/models/test_speaker.py
+++ tests/models/test_speaker.py
@@ -2,59 +2,63 @@ import unittest
 
 from models.speaker import SpeakerModel
 from tests.base_test import BaseTest
+from tests.model_test_data import TEST_SPEAKER_1
 
 
 class TestSpeaker(BaseTest):
     """Test all methods for the SpeakerModel"""
 
+    FIRST_NAME = TEST_SPEAKER_1["first_name"]
+    LAST_NAME = TEST_SPEAKER_1["last_name"]
+    EMAIL = TEST_SPEAKER_1["email"]
+    LINKEDIN_PROFILE = TEST_SPEAKER_1["linkedin_profile"]
+    GITHUB_PROFILE = TEST_SPEAKER_1["github_profile"]
+    TWITTER_PROFILE = TEST_SPEAKER_1["twitter_profile"]
+    BIO = TEST_SPEAKER_1["bio"]
+    PROFILE_PICTURE = TEST_SPEAKER_1["profile_picture"]
+
     def test_init(self):
         with self.app_context:
-            self.assertEqual(self.speaker_1.first_name, "Tomás")
-            self.assertEqual(self.speaker_1.last_name, "González")
-            self.assertEqual(self.speaker_1.email, "tgonz@python.org")
+            self.assertEqual(self.speaker_1.first_name, self.FIRST_NAME)
+            self.assertEqual(self.speaker_1.last_name, self.LAST_NAME)
+            self.assertEqual(self.speaker_1.email, self.EMAIL)
             self.assertEqual(
-                self.speaker_1.linkedin_profile,
-                "https://linkedin.com/in/tomas_gonzalez",
+                self.speaker_1.linkedin_profile, self.LINKEDIN_PROFILE
             )
             self.assertEqual(
-                self.speaker_1.github_profile,
-                "https://github.com/tomas_gonzalez",
+                self.speaker_1.github_profile, self.GITHUB_PROFILE
             )
             self.assertEqual(
-                self.speaker_1.twitter_profile,
-                "https://twitter.com/tomas_gonzalez",
+                self.speaker_1.twitter_profile, self.TWITTER_PROFILE
             )
+            self.assertEqual(self.speaker_1.bio, self.BIO)
             self.assertEqual(
-                self.speaker_1.bio, "Experto en Python y el uso de type hints"
-            )
-            self.assertEqual(
-                self.speaker_1.profile_picture,
-                "https://ppty.com/img/hA4oCfR&o17mqsXm.png",
+                self.speaker_1.profile_picture, self.PROFILE_PICTURE
             )
 
     def test_find_all(self):
         with self.app_context:
-            speaker_id = self.speaker_1.save_to_db().id
+            speaker = self.add_speaker_to_db(self.speaker_1)
 
             speakers = SpeakerModel.find_all()
 
-            self.assertEqual(speakers[0].id, speaker_id)
+            self.assertEqual(speakers[0].id, speaker.id)
 
     def test_find_by_email(self):
         with self.app_context:
-            speaker_email = self.speaker_1.save_to_db().email
+            speaker = self.add_speaker_to_db(self.speaker_1)
 
-            speaker = SpeakerModel.find_by_email(speaker_email)
+            speaker = SpeakerModel.find_by_email(speaker.email)
 
-            self.assertEqual(speaker.email, "tgonz@python.org")
+            self.assertEqual(speaker.email, self.EMAIL)
 
     def test_find_by_id(self):
         with self.app_context:
-            speaker_id = self.speaker_1.save_to_db().id
+            speaker = self.add_speaker_to_db(self.speaker_1)
 
-            speaker = SpeakerModel.find_by_id(speaker_id)
+            speaker = SpeakerModel.find_by_id(speaker.id)
 
-            self.assertEqual(speaker.first_name, "Tomás")
+            self.assertEqual(speaker.first_name, self.FIRST_NAME)
 
 
 if __name__ == "__main__":  # pragma: no cover
